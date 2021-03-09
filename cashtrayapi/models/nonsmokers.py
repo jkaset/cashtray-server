bc1e02c4ed4ac19a3870b18169faf1065928f25e
Jasmin Kaset <kasetski@gmail.com>
2021-03-09 17:49:24 -0600

    registration and login

--- cashtrayapi/models/nonsmokers.py
+++ cashtrayapi/models/nonsmokers.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 class Nonsmoker(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    quit_date = models.DateTimeField(auto_now=False, auto_now_add=True)
-    cigs_per_day = models.IntegerField()
-    price_per_pack = models.FloatField()
-    cigs_per_pack = models.IntegerField()
-    start_smoking_year = models.DateField(auto_now=False, auto_now_add=False)
+    quit_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True)
+    cigs_per_day = models.IntegerField(null=True)
+    price_per_pack = models.FloatField(null=True)
+    cigs_per_pack = models.IntegerField(null=True)
+    start_smoking_year = models.DateField(auto_now=False, auto_now_add=False, null=True)
